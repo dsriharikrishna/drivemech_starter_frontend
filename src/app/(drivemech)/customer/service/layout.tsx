9edbafd9014c3c4d8c248a4706a281b08d663dfa
dsriharikrishna <dsriharikrishna2002@gmail.com>
2025-11-26 20:41:20 +0530

    Update layout.tsx

--- src/app/(drivemech)/customer/service/layout.tsx
+++ src/app/(drivemech)/customer/service/layout.tsx
@@ -1,9 +1,11 @@
+import Navbar from '@/components/Layout/Navbar';
 import { ReactNode } from 'react';
 
 export default function ServiceLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <main className="py-8">
+      <Navbar />
+      <main className="p-4 m-4 py-12">
         {children}
       </main>
     </div>
